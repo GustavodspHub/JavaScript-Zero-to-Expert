36506c63d985fff9fadbd23c65fb75d97a1acd4d
Gustavo Pereira <gustavopereira0979@gmail.com>
2024-02-03 03:57:21 -0300

    feat: A brief introduction to JavaScript

---
+++ JavaScript-Fundamentals-Part-1/a-brief-introduction-to-javascript.js
@@ -0,0 +1,19 @@
+/**
+  JavaScript é uma linguagem de programação de alto nível,
+    Orientada a Objeto e multi paradigmas.
+
+  Afinal, o que isso quer dizer?
+
+  Linguagem de programação - É uma linguagem escrita que especifica um conjunto
+    de instruções e regras usadas para criar programas (Software).
+
+  Alto nível - Todos os Software criados precisam de recursos, como processador e memória.
+    Em linguagens de baixo nível, esses recursos precisam ser gerenciados de uma maneira
+      manual pelo desenvolvedor. Em linguagens de alto nível, esses recursos são gerenciados
+      pela própria linguagem.
+
+  Orientada a Objeto - Linguagem baseada em objetos para armazenas tipos de dados.
+
+  Multi Paradigmas - É possível ter vários estilos de código em JavaScript.
+
+ */
